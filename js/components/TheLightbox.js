174185e331328295d259bf23eb601036a8250394
Joanna C <joannachowhy@gmail.com>
2022-11-14 21:46:54 -0500

    create vue components

---
+++ js/components/TheLightbox.js
@@ -0,0 +1,34 @@
+export default {
+    name: 'TheLightboxComponent',
+
+    props: {
+        car: Object
+    },
+
+    template: `
+    <div class="info-container">
+        <div class="info-text">
+            <div class="models-basic">
+                <div>
+                    <h3>The 2023 Mini {{car.name}} Convertible</h3>
+                    <p>All-Inclusive Price: {{car.price}}</p>
+                </div>
+                <div class="text-wrapper">
+                    <div><p><span>Lease</span><br>{{car.lease}}</p></div>
+                    <div><p><span>Finance</span><br>{{car.finance}}</p></div>
+                </div>
+            </div>
+            <div class="models-features">
+                <ul>
+                    <li>{{car.features}}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="info-img">
+            <img :src='"images/" + car.pic' alt="vehicle image">
+        </div>
+    </div>
+    `,
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,41 @@
+// import components first!!!
+import CarThumb from './components/TheCarThumbnail.js';
+import LightBox from './components/TheLightbox.js';
+
+(() => {
+    // create vue instance here
+    const { createApp } = Vue
+
+    createApp({
+        created() {
+            // get your remote data here 
+            fetch('./data.json')
+                .then(res => res.json()) // convert the data into a plain js object
+                .then(data => this.carData = data) // convert to things we can use
+            .catch(error => console.error(error));
+        },
+
+        data() {
+            return {
+                carData: {},
+                lbData: {},
+                showLB: false
+            }
+        },
+
+        components: {
+            thecarthumb: CarThumb,
+            thelightbox: LightBox
+        },
+
+        methods: {
+            loadLightbox(car) {
+                // set the lightbox data/ render it
+                this.lbData = car;
+
+                //show the lightbox
+                this.showLB = true;
+            }
+        }
+    }).mount('#app')
+})()
